88c9a12b47d63b8bd2b26e45681b1e4b7ed66795
felipe moreira <fmsantos1997@hotmail.com>
2022-11-17 02:45:19 -0300

    changes

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
-# Your interactive and bindable Audio Mixer
+# Your audioapp for process volume control
 
 ## Using: 
 
 - [Pycaw](https://github.com/AndreMiras/pycaw)
 - [Tkinter Slider](https://www.pythontutorial.net/tkinter/tkinter-slider/)
-- [Possible Audio Library: ossaudiodev](https://docs.python.org/3/library/ossaudiodev.html#module-ossaudiodev)
-    - [Is it Deprecated?](https://peps.python.org/pep-0594/#ossaudiodev)
-- [Possible Audio Library: sounddevice](https://python-sounddevice.readthedocs.io/en/0.4.5/installation.html)
 
 ## Purpose
 
--- mixer.py
+++ mixer.py
@@ -1,21 +1,81 @@
-from ctypes import cast, POINTER
-from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume
+import tkinter as tk
+from tkinter import StringVar, ttk, DoubleVar
+from random import randint
 
-from tkinter import ttk
+INTERVAL = 2000  # 2 seconds
+
+class WindowsAudioProcess:
+    def __init__(self, name, frame) -> None:
+        self.name = name
+        self.volume = DoubleVar()
+        self.frame = frame
+
+    def create_panel(self, x: int):
+        name = ttk.Label(self.frame, text=self.name,foreground='white', background='black')
+        name.grid(row=2, column=x)
+
+        scale = ttk.Scale(self.frame, from_=1, to=0, orient='vertical', variable=self.volume)
+        scale.grid(row=3, column=x, sticky="NEWS")
+
+        value = ttk.Label(self.frame, text=f"{self.volume.get():.2f}", foreground="red", font=("Arial", 15))
+        value.grid(row=4, column=x)
+
+    def set_volume(self, volume: float):
+        self.volume = volume
+
+    def get_volume(self) -> float:
+        return self.volume
 
 
-MAX = 1
-MIN = .5
+class VolumeControllerApp:
+    def __init__(self) -> None:
+        self.processes = []
+        self.root = tk.Tk()
+        self.root.geometry("720x480")
+        self.root.resizable(False, False)
+        self.root.title("Windows Volume Controller")
 
-def main():
-    sessions = AudioUtilities.GetAllSessions()
-    for session in sessions:
-        volume = session._ctl.QueryInterface(ISimpleAudioVolume)
-        if session.Process and session.Process.name() == "chrome.exe":
-            print(f"volume.GetMasterVolume(): {volume.GetMasterVolume()} and {session.Process.name()}")
-            volume.SetMasterVolume(MAX, None)       
+        self.mainframe = tk.Frame(self.root, background="black")
+        self.mainframe.pack(fill='both', expand=True)
+        self.process_names = self.create_process_list()
+
+        self.random = ttk.Label(
+            self.mainframe, text="", background='black', foreground='white', font=("Arial", 30))
+        self.random.grid(row=0, column=0, sticky="NEWS")
+
+        self.update()
+
+        self.root.mainloop()
+        return
+
+    def update(self):
+        self.random.config(text=randint(0, 100))
+        self.create_panel()
+        self.root.after(INTERVAL, self.update)
+
+    def create_panel(self):
+        for column, process in enumerate(self.processes):
+            process.create_panel(column)
+
+    def create_process_list(self) -> None:
+        for process in self.get_process_names():
+            self.processes.append(WindowsAudioProcess(
+                process.split('.')[0], self.mainframe))
+        return self.processes
+
+    def get_process_names(self) -> list:
+        process_names = []
+        sessions = AudioUtilities.GetAllSessions()
+        for session in sessions:
+            if session.Process and session.Process.name():
+                process_names.append(session.Process.name())
+        return process_names
 
 
 if __name__ == "__main__":
-    main()
+    VolumeControllerApp()
+
+#     volume = session._ctl.QueryInterface(ISimpleAudioVolume)
+#         f"volume.GetMasterVolume(): {volume.GetMasterVolume()} and {type(session.Process.name())}")
+#     volume.SetMasterVolume(1, None)
